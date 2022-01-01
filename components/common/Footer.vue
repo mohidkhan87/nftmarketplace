cf0ea995f0da28ce34feece4ad18594f565b9dbb
UsmanMurtaza92 <murtazausman041@gmail.com>
2022-01-01 19:29:02 +0500

    collection_page_done

---
+++ components/common/Footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="bg-white bg-opacity-5 py-20">
+    <Container>
+      <div class="grid grid-cols-6 ">
+        <div class=" col-span-2">
+          <img src="@/assets/img/header/logo.svg" alt="logo" class="mt-2 mb-4">
+          <div class="w-1/2 text-sm font-thin text-white leading-4 italic">
+            Lorem ipsum, dolor sit amet adipisicing elit. Quaerat, deserunt.
+          </div>
+        </div>
+        <div class=" text-white ">
+          <h2 class="mb-4 italic font-medium ">
+            Join the Community
+          </h2>
+          <div class="flex gap-3">
+            <img src="@/assets/img/footer/discord.svg" alt="dicord">
+            <img src="@/assets/img/footer/twitter.svg" alt="twitter">
+            <img src="@/assets/img/footer/instagram.svg" alt="instagram">
+          </div>
+        </div>
+        <div class=" text-white ">
+          <div class=" w-max mx-auto">
+            <h2 class="mb-4 italic font-medium ">
+              Marketplace
+            </h2>
+            <ul class="flex flex-col gap-3 text-sm font-thin italic">
+              <li>Collections</li>
+              <li>Stats</li>
+            </ul>
+          </div>
+        </div>
+        <div class=" text-white ">
+          <div class=" w-max mx-auto">
+            <h2 class="mb-4 italic font-medium ">
+              Account
+            </h2>
+            <ul class="flex flex-col gap-3 text-sm font-thin italic">
+              <li>Collections</li>
+              <li>Stats</li>
+              <li>Collections</li>
+              <li>Stats</li>
+              <li>Collections</li>
+              <li>Stats</li>
+            </ul>
+          </div>
+        </div>
+        <div class=" text-white ">
+          <div class=" w-max mx-auto">
+            <h2 class="mb-4 italic font-medium ">
+              Company
+            </h2>
+            <ul class="flex flex-col gap-3 text-sm font-thin italic">
+              <li>Collections</li>
+              <li>Stats</li>
+              <li>Collections</li>
+              <li>Stats</li>
+              <li>Stats</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Container>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="">
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#0985ac',
+        secondary: '#044c62',
+        greenish: '#01CDDF',
+        skyBlue: '#01AEE5',
+        borderBlue: '#459eba',
+        lightBlue: '#cfdee1'
+      },
+      boxShadow: {
+        rounded: '0 0 13px 1px rgba( 0, 0, 0, 0.2 )'
+      }
+
+    }
   },
   variants: {
     extend: {}
